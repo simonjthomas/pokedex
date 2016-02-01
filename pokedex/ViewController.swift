8180ef3a4e6cf02f22d383394e2a95ddbc89a40b
Simon Thomas <simonjthomas@gmail.com>
2016-02-01 01:16:04 +0000

    Added required delegate methods for collection view and added test data

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -8,17 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    // Outlet for collection view
+    @IBOutlet weak var collection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        // Set delegate for collection view so we can implement delegate methods
+        collection.delegate = self
+        collection.dataSource = self
 
     }
 
+    // Delegate methods required for collection view
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        // Dequeue for reusing cells to prevent having to create them all the time and using lots of memory
+        // indexPath is the current cell number
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
+            // If able to grab a cell of the correct type, return it
+            
+            // indexPath.row is current cell number, not actual row in traditional sense
+            // Create a new pokemon object for each entry.
+            let pokemon = Pokemon(name: "Test", pokedexId: indexPath.row)
+            cell.configureCell(pokemon)
+            
+            return cell
+        } else {
+            // If can't retrieve an existing cell, make a new one and return that.
+            return UICollectionViewCell()
+        }
+        
+    }
+
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
     
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Define number of items in the secion
+        return 718
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
 
-
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        // Define size of cells
+        // Can make this dynamic if need be
+        return CGSizeMake(105,105)
+    }
 }
 
