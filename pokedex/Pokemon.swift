a05d4599ab5504793a3672d64447e3a5a9e2d8fd
Simon Thomas <simonjthomas@gmail.com>
2016-02-02 01:34:32 +0000

    Connected outlets and added additional attributes to pokemon class

--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -13,6 +13,12 @@ class Pokemon {
     // Can use "!" to guarantee there's always a value as it's required in the initialiser
     private var _name: String!
     private var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionTxt: String!
     
     // Getters
     var name: String {
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -12,6 +12,22 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +42,8 @@ class PokemonDetailVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
 
 }
